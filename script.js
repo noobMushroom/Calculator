b1c30ff60ddc9fb118ce9cbc8260f97da094d66c
mushroom <mushroom019.coder@gmail.com>
2022-09-20 15:52:53 +0530

    ok i fixed all the problems just two more problems backspace button and sum

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-// it's a master function which will take all the numbers and decide what to do with them 
+//* it's a master function which will take all the numbers and decide what to do with them 
 
-let sum=""
+let sum = ""
 let num1 = ''
 let num2 = ''
 
 
 const master = (number1, number2, operator) => {
     if (operator === "+") {
-        sum= number1 + number2;
-        num1Display.textContent= sum;
-        num1= sum
-        hope2()
+        sum = number1 + number2;
+        num1Display.textContent = sum;
+        num1 = sum
+        num2 = ''
         return sum;
-        
+
     } else if (operator === "-") {
-        sum= number1 - number2;
-        num1Display.textContent= sum;
-        num1= sum
-        hope2()
+        sum = number1 - number2;
+        num1Display.textContent = sum;
+        num1 = sum
+        num2 = ''
         return sum;
     } else if (operator === "*") {
-        sum= number1 * number2;
-        num1Display.textContent= sum;
-        num1= sum
-        hope2()
+        sum = number1 * number2;
+        num1Display.textContent = sum;
+        num1 = sum
+        num2=""
         return sum;
     } else if (operator === "/") {
         if (number2 === 0) {
-            sum="Error you can't divide a number by zero"
-            return  sum
+            sum = "Error you can't divide a number by zero"
+            return sum
         } else {
-            sum = (number1/ number2).toFixed(2)
-            num1= sum
+            sum = (number1 / number2).toFixed(2)
+            num1 = sum
+            num2 = ''
             num1Display.innerHTML = sum
-            hope2()
             return sum
         }
     }
 }
- 
 
 
-// grabbing important buttons and display. 
+
+// *grabbing important buttons and display. 
 const numBtn = document.querySelectorAll('.number')
 const opBtn = document.querySelectorAll('.operator')
 const ans = document.getElementById('ans')
@@ -51,10 +51,13 @@ const operatorDiv = document.getElementById('operatorDiv')
 const clearbtn = document.getElementById('clear')
 
 
-// function numbers is important function it takes number and them to num 1 and num 2 and show them to display
+// *function numbers is important function it takes number and them to num 1 and num 2 and show them to display
+
+
+
 function numbers() {
+    
     something()
-    // hope();
     numBtn.forEach(button => button.addEventListener('click', () => {
         if (some == '') {
             num1 += button.value
@@ -68,13 +71,18 @@ function numbers() {
     }))
 }
 
-// something returns operator and helps to switch between num1 and num2 
+// *something returns operator and helps to switch between num1 and num2 
 // todo: change it's name
-let some = ""
-let op = ''
-function something() {
 
+
+
+let some = ""
+let op = '' // *this is the operator 
+
+function something() {
+    
     opBtn.forEach(button => button.addEventListener('click', () => {
+        operator()
         some = button.name
         op = button.value
         operatorDiv.innerHTML = button.value
@@ -83,26 +91,36 @@ function something() {
 }
 
 
-// and finally this calls innerHTML to master and calculate everything 
+// *and finally this calls innerHTML to master and calculate everything 
 
 const equal = document.getElementById('equal')
 equal.addEventListener("click", () => {
     master(Number(num1), Number(num2), op)
     num2Display.textContent = ''
     operatorDiv.textContent = ''
-    ans.innerHTML=sum
+    num1Display.innerHTML=''
+    operatorDiv.innerHTML=''
+    ans.innerHTML = sum
     return num2 = ''
 })
 
-function hope(){
-    
-    master(Number(num1), Number(num2), op)
-    
-    
-}
-
-function hope2(){
-    num2=''
-}
 
 numbers()
+
+
+
+// todo somehow take the sum and then pass it to the another function and every time i click operators i add to it 
+
+
+function operator() {
+        master(Number(sum), Number(num2), op)
+        console.log("this is num2",num2 , "and this is num1" , num1)
+        // num2 = ''
+        num2Display.textContent = ''
+        operatorDiv.textContent = op
+        console.log("you called me")
+}
+
+
+
+//! no2 will be somehow fix the sum
